8ec3caa5dd8ce671388a6ff2e22238b4c30adf63
Pringelo <oliwierfer@gmail.com>
2025-04-28 19:24:52 +0200

    lsrp

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 // This script is optional and can be used to add interactivity to your website
 // For now, we will keep it simple
-console.log("Welcome to Los Santos Role Play! TEST");
+console.log("Welcome to Los Santos Role Play!");
